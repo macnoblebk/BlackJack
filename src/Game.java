1c765a4b0a8e93dbe85f5e82ac64f26cad99d9eb
macnoblebk <macnoblebk@gmail.com>
2023-08-06 13:30:27 -0700

    Creates Game model and adds function to generate card deck for game play

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,36 @@
-public class GamePlay {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Game {
+    private final int NUMBER_OF_PLAYERS;
+    private boolean isGameOver;
+    private ArrayList<Card> playerHand;
+    private Stack<Card> cardDeck;
+    private Queue<ArrayList<Card>> playerQueueArray;
+
+    public Game() {
+        this.NUMBER_OF_PLAYERS = 3;
+        this.isGameOver = false;
+        this.cardDeck = getCardDeck();
+        //getInitialHand();
+    }
+
+    private Stack<Card> getCardDeck() {
+        ArrayList<Card> list = new ArrayList<>();
+        Stack<Card> cardStack = new Stack<>();
+        for (Card.Suit cs : Card.Suit.values()){
+            for (Card.Value cv : Card.Value.values()){
+                list.add(new Card(cs, cv.getValue()));
+            }
+        }
+
+        Collections.shuffle(list);
+        cardStack.addAll(list);
+
+        return cardStack;
+    }
+
+
 }
