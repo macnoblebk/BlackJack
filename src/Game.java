a8bc65a85e377f2048acf07db64a38340b0b26cf
macnoblebk <macnoblebk@gmail.com>
2023-08-06 22:20:13 -0700

    Implements functionality to add player names, defualt hit strategy and draw method

--- src/Game.java
+++ src/Game.java
@@ -2,17 +2,43 @@ import java.util.*;
 
 public class Game {
     private final int NUMBER_OF_PLAYERS;
+    private final String[] PLAYER_NAME_ARRAY;
     private boolean isGameOver;
-
     private Stack<Card> cardDeck;
     private Queue<ArrayList<Card>> playerArrayLinkedList;
 
-    public Game() {
+    public Game(Scanner keyboard) {
         this.NUMBER_OF_PLAYERS = 3;
+        PLAYER_NAME_ARRAY = new String[NUMBER_OF_PLAYERS];
+        populatePlayerNameArray(keyboard);
         this.isGameOver = false;
         this.cardDeck = getCardDeck();
         playerArrayLinkedList = new LinkedList<>();
         initializePlayerQueue();
+    }
+
+    private void populatePlayerNameArray(Scanner keyboard){
+        for(int i = 0; i < NUMBER_OF_PLAYERS; i++){
+            PLAYER_NAME_ARRAY[i] = getPlayerName(keyboard);
+        }
+    }
+
+    private String getPlayerName(Scanner keyboard) {
+        String input;
+        do {
+            System.out.print("Enter player's name : ");
+            input = keyboard.nextLine();
+            if (input.isEmpty())
+            {
+                System.out.println("Player name cannot be empty!");
+            }
+            else if (input.isBlank())
+            {
+                System.out.println("Player name cannot be only whitespaces!");
+            }
+        }
+        while (input.isBlank());
+        return input;
     }
 
     private Stack<Card> getCardDeck() {
@@ -31,14 +57,26 @@ public class Game {
     }
 
     private void initializePlayerQueue(){
-        for (int i = 0; i < NUMBER_OF_PLAYERS; i++){
+        for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
             playerArrayLinkedList.add(new ArrayList<>());
         }
+
+    }
+
+    private void draw() {
         for (ArrayList<Card> hand : playerArrayLinkedList) {
             hand.add(cardDeck.pop());
             hand.add(cardDeck.pop());
         }
     }
 
+    private void gamePlay() {
+        draw();
+
+    }
+
+    private void defaultHitStrategy(ArrayList<Card> hand) {
+        hand.add(cardDeck.pop());
+    }
 
 }
