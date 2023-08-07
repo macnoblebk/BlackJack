45d1c5fde93f3f0d9191e56f330b59f71c38f071
macnoblebk <macnoblebk@gmail.com>
2023-08-07 10:46:21 -0700

    Entry point for black jack game

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        Game game = new Game(keyboard);
+        game.gamePlay();
+    }
 }
